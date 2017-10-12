04bc4ff2ed6d2ff0448f4a93b3cf8177b8ba33ae
Milou Schelling <milou.schelling@gmail.com>
2017-10-12 15:47:42 +0200

    Fix promo

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-get "/pages/:page" => "pages#show"
+  get '/pages/:page' => 'pages#show'
 
-root "pages#show", page: "home"
+  root 'pages#show', page: 'home'
 
-<<<<<<< HEAD
-get "promo/beraber", to: 'pages#promo'
-=======
-get "promo/sjamsoedin-orion-ep", to: 'pages#promo'
->>>>>>> 051e1bfb2ad43eefe9306c99e479bd4544f77049
+  get 'promo/beraber', to: 'pages#promo'
 end
